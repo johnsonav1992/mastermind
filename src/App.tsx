abe9b6b23069cc5f05b292baa2d32ae32d0f5173
Alex Johnson <johnsonav1992@gmail.com>
2025-06-03 05:41:23 -0500

    feat: implement GameBoard component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import GameBoard from './components/GameBoard';
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <h1>Mastermind</h1>
+      <p>Can you crack the code?</p>
+      <GameBoard />
+    </div>
+  );
 }
 
 export default App;
