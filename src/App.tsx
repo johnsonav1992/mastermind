163e8f657371d68ee79bcc5ad56e7058a10a9ae0
Alex Johnson <johnsonav1992@gmail.com>
2025-06-04 19:20:31 -0500

    pigment css!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { css } from '@pigment-css/react';
 import GameBoard from './components/GameBoard/GameBoard';
 
 function App() {
 	return (
-		<div className='main-container'>
+		<div
+			className={css({
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				height: '100vh',
+				gap: '1rem'
+			})}
+		>
 			<h1>Mastermind</h1>
 			<p>Can you crack the code?</p>
 			<GameBoard />
