872703b4f16769f276675e935e212ad8bf98a609
Alex Johnson <johnsonav1992@gmail.com>
2025-06-06 05:42:34 -0500

    secret code area converted

--- src/components/SecretCodeArea/SecretCodeArea.tsx
+++ src/components/SecretCodeArea/SecretCodeArea.tsx
@@ -1,19 +1,43 @@
 import { useAtomValue } from 'jotai';
 import { pegColors } from '../../constants/pegColors';
-import styles from './SecretCodeArea.module.css';
 import { secretCodeAtom } from '../../state/atoms';
 import { emptyPegStyle, pegStyle } from '../../styles/globalStyles';
+import { css } from '../../../styled-system/css';
 
 const SecretCodeArea = () => {
 	const secretCode = useAtomValue(secretCodeAtom);
-	const isGameInProgress = false;
+	const isGameInProgress = true;
 
 	console.log({ secretCode });
 
 	return (
-		<div className={styles.codeRow}>
+		<div
+			className={css({
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				gap: '10px',
+				marginBottom: '16px',
+				borderRadius: '6px',
+				border: '2px solid #3a1d0d',
+				width: '150px',
+				height: '40px',
+				marginLeft: '68px'
+			})}
+		>
 			{isGameInProgress ? (
-				<div className={styles.codeCover} />
+				<div
+					className={css({
+						width: '150px',
+						height: '36px',
+						backgroundColor: '#5c3312',
+						borderRadius: '4px',
+						boxShadow: 'inset 0 -2px 4px rgba(0, 0, 0, 0.4)',
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center'
+					})}
+				/>
 			) : (
 				<>
 					{secretCode.map((peg, i) => (
